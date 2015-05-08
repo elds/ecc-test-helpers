4dc3f590c2c8438b433969e3b3d7ef0171d97f4c
Tim Ermilov <tim.ermilov@eccenca.com>
2015-05-08 11:53:28 +0200

    add location mocking for apps testing, add main container div, adjust eslint ignores

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ import Intl from 'intl';
 
 // say we're not in webpack environment
 // this is required to skip including styles
-global.__WEBPACK__ = false;
+global.__WEBPACK__ = false; // eslint-disable-line no-underscore-dangle
 
 // init jsdom
-global.document = jsdom();
+global.document = jsdom('<html><body><div id="mainContainer"></div></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
+
+// mock location
+global.window.location.href = '/';
 
 // local storage polyfill
 global.window.localStorage = localStorage;
